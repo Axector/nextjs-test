0024e55028bfe6b82957818001ffd30b3e1ee789
Axector <dansvjat021100@gmail.com>
2026-01-30 00:12:34 +0200

    - link href fix

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,15 +1,9 @@
 import Link from "next/link";
-import nextConfig from "@/../next.config.mjs";
 import './styles.scss';
 
 export const CustomLink = ({ children, ...props }) => {
-  const newProps = {
-    ...props,
-    href: `${nextConfig.basePath}${props.href}`,
-  };
-
   return (
-    <Link {...newProps}>
+    <Link {...props}>
       <span>{children}</span>
     </Link>
   );
